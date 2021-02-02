fbbd5fe2df12a845e6d980bfa29e06003458731e
weiy-crypto <2605009464@qq.com>
2021-02-03 03:10:34 +0800

    完成了People类的初始化功能

--- src/test/java/com/example/frist_test/FristTestApplicationTests.java
+++ src/test/java/com/example/frist_test/FristTestApplicationTests.java
@@ -37,4 +37,21 @@ class FristTestApplicationTests {
         People people = new People();
         System.out.println(people);
     }
+
+    @Test
+    void testFn3() {
+        People people = new People();
+        people.setName("zhangsan");
+        people.setAge(28);
+        Dog dog = new Dog();
+        dog.setName("aaa");
+        dog.setAge(12);
+        dog.setColor("red");
+        Cat cat = new Cat();
+        cat.setName("bbb");
+        cat.setAge(15);
+        cat.setColor("pink");
+        cat.setType("w");
+        System.out.println(people);
+    }
 }
